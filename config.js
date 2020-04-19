dbdd14f7c6d5ba6d79f949e5b38aec7308860063
Daniel Guzman <daniel.e.guzman.s@gmail.com>
2020-04-19 15:30:19 -0400

    fix on db url prod

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 if(process.ENV === 'PROD'){
-    DATABASE_URL = 'process.ENV.DATABASE_URL'
+    DATABASE_URL = process.ENV.DATABASE_URL
 }else{
     DATABASE_URL = 'mongodb://localhost/angular-auth'
 }
